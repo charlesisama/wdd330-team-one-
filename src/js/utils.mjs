18fe5d4e9edf5d352f80ea39647b80185ec510c9
charlesisama <cisama@byupathway.edu>
2026-09-17 08:57:33 +0100

    Add comment to clarify renderListWithTemplate function purpose

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -30,6 +30,7 @@ export function getParam(param) {
   return product
 }
 
+// render a list of items using a template function
 export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(template);
   // if clear is true we need to clear out the contents of the parent.
