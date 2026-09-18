2028bedc2763d57bbf2a90918f4a3db66124f42b
charlesisama <cisama@byupathway.edu>
2026-09-18 09:50:02 +0100

    Add product listing page and associated JavaScript functionality

---
+++ src/js/product_listing.js
@@ -0,0 +1,17 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+async function init() {
+  await loadHeaderFooter();
+
+  const dataSource = new ProductData("tents");
+
+  const element = document.querySelector(".product-list");
+
+  const listing = new ProductList("Tents", dataSource, element);
+
+  listing.init();
+}
+
+init();
